571cd4a71ec3d6829c982da9d160de7c35ff4f74
beck410 <beck410@hotmail.com>
2015-04-02 11:31:44 -0500

    added exception handling tutorial

---
+++ exceptional.py
@@ -0,0 +1,11 @@
+'''A module for demonstratig exceptions.'''
+
+def convert(s):
+    '''Convert to an integer.'''
+    x = -1
+    try:
+        x = int(s)
+        print('Conversion succeeded! x =', x)
+    except (ValueError, TypeError):
+        print('Conversion failed!')
+    return x
